30f22c9ce1d7d0bb3f5a2343a6ec08e024118afa
unknown <klencodes@gmail.com>
2021-03-03 03:21:37 +0000

    add heroku remote

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://loansmanager.herokuapp.com',
+    origin: 'http://localhost:4200',
     methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST', 'OPTIONS'],
     allowedHeaders: 'Content-Type, Authorization, XMLHttpRequest, Origin, X-Requested-With, Accept',
     credentials: true
@@ -21,11 +21,15 @@ app.use('/accounts', require('./routes/accounts/account.controller'));
 app.use('/loans', require('./routes/loans/loan.controllers'));
 app.use('/payments', require('./routes/payments/payment.controller'));
 
+/* Search Existing Customers */
+app.get('/', function (req, res, next) {
+    res.send('respond with a resource OK');
+  });
 // global error handler
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 8080) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
 });
