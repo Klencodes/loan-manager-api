f79177de7d92a3e210bf66f2e67bc0776be7e627
unknown <klencodes@gmail.com>
2021-03-03 02:56:44 +0000

    add heroku remote

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: 'https://loan-backend-api.herokuapp.com',
     methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST', 'OPTIONS'],
     allowedHeaders: 'Content-Type, Authorization, XMLHttpRequest, Origin, X-Requested-With, Accept',
     credentials: true
@@ -25,7 +25,7 @@ app.use('/payments', require('./routes/payments/payment.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 8080) : 4000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
 });
